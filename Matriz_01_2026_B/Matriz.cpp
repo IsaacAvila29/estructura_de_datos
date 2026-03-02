63c6d8b18c5428a321f74d44e0e53df9cab6162b
IsaacAvila29 <isaac.avila.saenz29@gmail.com>
2026-03-02 12:46:39 -0700

    Add Matriz class implementation with matrix operations and main program setup

---
+++ Matriz_01_2026_B/Matriz.cpp
@@ -0,0 +1,92 @@
+#include "Matriz.h"
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+//******** Matriz.cpp **************
+inline Matriz::Matriz (int col, int fil )
+{
+    columna=col;
+    fila=fil;
+    llenarMatriz();
+}
+inline void Matriz::llenarMatriz ( )
+{
+    //se desarrolla por el equipo
+    // usando las funciones para generar números
+    // aleatorios
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    for(int i=0;i<fila;i++){
+        for(int j=0;j<columna;j++){
+            matriz[i][j] = std::rand() % 10; // valores 0..9
+        }
+    }
+}
+
+inline bool Matriz::SumarMatriz( Matriz M2, Matriz & Result)
+{
+    if(fila!=M2.fila || columna!=M2.columna)
+    {
+        return false;
+    }
+    for(int i=0;i<fila;i++){
+        for(int j=0;j<columna;j++){
+            Result.matriz[i][j]= this->matriz[i][j] + M2.matriz[i][j];
+        }
+    }
+    Result.fila = fila;
+    Result.columna = columna;
+    return true;
+}
+
+inline bool Matriz::RestarMatriz(Matriz M2, Matriz& Result) {
+    if(fila!=M2.fila || columna!=M2.columna)
+    {
+        return false;
+    }
+    for(int i=0;i<fila;i++){
+        for(int j=0;j<columna;j++){
+            Result.matriz[i][j]= this->matriz[i][j] - M2.matriz[i][j];
+        }
+    }
+    Result.fila = fila;
+    Result.columna = columna;
+    return true;
+}
+
+inline bool Matriz::Multiplicar(Matriz M2, Matriz& Result) {
+    if(columna != M2.fila) {
+        return false; // dimensiones incompatibles
+    }
+    // Resultado: filas de this x columnas de M2
+    int rFil = fila;
+    int rCol = M2.columna;
+    // inicializar a 0
+    for(int i=0;i<rFil;i++){
+        for(int j=0;j<rCol;j++){
+            Result.matriz[i][j] = 0;
+        }
+    }
+    for(int i=0;i<rFil;i++){
+        for(int j=0;j<rCol;j++){
+            for(int k=0;k<columna;k++){
+                Result.matriz[i][j] += this->matriz[i][k] * M2.matriz[k][j];
+            }
+        }
+    }
+    Result.fila = rFil;
+    Result.columna = rCol;
+    return true;
+}
+
+void Matriz::Imprimir() {
+    for (int i = 0; i < fila; i++) {
+        for (int j = 0; j < columna; j++) {
+            cout << matriz[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
